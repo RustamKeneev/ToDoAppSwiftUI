452474ccaaa34e1e87795f57260025c75a45b17c
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-03-23 22:02:53 +0600

    Initial Commit

---
+++ ToDoAppSwiftUI/ToDoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppSwiftUIApp.swift
+//  ToDoAppSwiftUI
+//
+//  Created by Rustam Keneev on 23/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
